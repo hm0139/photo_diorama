b3c5ec8050e5dd93eff0d2a9dee9cdf62d874480
hm0139 <m.kyohei0114@gmail.com>
2023-07-26 12:59:08 +0900

    入力されたユーザ情報の登録機能追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  private
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:user_name, :name, :reading_name, :postal_code, :prefectures, :city, :building_name, :financial_institution, :branch, :deposit, :account_number, :account_holder])
+  end
 end
