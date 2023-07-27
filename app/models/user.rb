696e6fc1fc28e7ae1ea4238bfefa6a621519b69b
hm0139 <m.kyohei0114@gmail.com>
2023-07-27 21:41:58 +0900

    バリデーションを設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,18 @@ class User < ApplicationRecord
 
   has_many :commissions
   has_many :notifications
+
+  with_options presence: true do
+    validates :user_name
+    validates :name
+    validates :reading_name, format: {with: /\A[ァ-ヴー]+\z/, message: "は全角カタカナで記入してください"}
+    validates :postal_code, format: {with: /\A\d{3}-\d{4}\z/, message: "が正しくありません"}
+    validates :prefectures, numericality: {other_than: 0}
+    validates :city
+  end
+
+  #注 : 以下のバリデーションは空白を許可
+  validates :deposit, numericality: true
+  validates :account_number, format: {with: /\A\d{4,7}\z|\A\z/ , message: "は正しく入力してください"}
+  validates :account_holder, format: {with: /\A[ァ-ヴー]+\z|\A\z/, message: "は全角カタカナで記入してください"}
 end
