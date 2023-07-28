e7be7353d375b207c7d908f38765dca3fcba2a0c
hm0139 <m.kyohei0114@gmail.com>
2023-07-28 22:57:29 +0900

    依頼を受けたら通知からは削除と一覧から非表示に

--- app/controllers/dealings_controller.rb
+++ app/controllers/dealings_controller.rb
@@ -7,6 +7,9 @@ class DealingsController < ApplicationController
     @dealing = Dealing.create(user_id: current_user.id, commission_id: params[:commission_id])
     @commission = @dealing.commission
     @commission.update(status: Commission.statuses[:dealing])
+    if @commission.directly
+      @commission.notification.destroy
+    end
     redirect_to commission_dealing_path(@dealing.commission.id,@dealing)
   end
 end
--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,5 +1,5 @@
 class NotificationsController < ApplicationController
   def index
-    @commissions = Commission.where(directly: true).where(contractor_id: current_user.id)
+    @commissions = Commission.where(directly: true).where(contractor_id: current_user.id).where(status: Commission.statuses[:undealt])
   end
 end
