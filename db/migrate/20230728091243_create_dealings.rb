025fc21b655e8a326166af1780ea83c59a847ede
hm0139 <m.kyohei0114@gmail.com>
2023-07-28 18:20:02 +0900

    dealingモデルの作成

---
+++ db/migrate/20230728091243_create_dealings.rb
@@ -0,0 +1,10 @@
+class CreateDealings < ActiveRecord::Migration[7.0]
+  def change
+    create_table :dealings do |t|
+      t.integer :status
+      t.references :commission, null: false, foreign_key: true
+      t.references :user, null: false, foreign_key: true
+      t.timestamps
+    end
+  end
+end
