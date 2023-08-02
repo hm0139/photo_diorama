9ccd4b914369a6792a24d40057a236cd7fba2c5c
hm0139 <m.kyohei0114@gmail.com>
2023-08-02 16:56:46 +0900

    パスとアクション内の処理の修正

--- app/controllers/creators_controller.rb
+++ app/controllers/creators_controller.rb
@@ -2,7 +2,6 @@ class CreatorsController < ApplicationController
   def index
     user_id = user_signed_in? ? current_user.id : nil
     @creators = User.where(kind: 1).where.not(id: user_id)
-    @commission = Commission.find(params[:id]) if params.has_key?(:id)
   end
 
   def search
