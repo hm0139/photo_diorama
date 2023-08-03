70b845466e4d0fc14b735f83a9776def8281051e
hm0139 <m.kyohei0114@gmail.com>
2023-08-03 18:24:50 +0900

    依頼者と引受人以外は取引ページに入れないように

--- app/controllers/dealings_controller.rb
+++ app/controllers/dealings_controller.rb
@@ -1,5 +1,6 @@
 class DealingsController < ApplicationController
   before_action :redirect_root, except: :show
+  before_action :show_redirect_root, only: :show
 
   def new
     @commission = Commission.find(params[:commission_id])
@@ -19,13 +20,19 @@ class DealingsController < ApplicationController
 
   def show
     @chat = Chat.new
-    @commission = Commission.find(params[:commission_id])
     @chats = Chat.merge(Chat.where(user_id: @commission.contractor_id).or(Chat.where(user_id: @commission.user_id))).where(dealing_id: @commission.dealing.id)
   end
 
   private
   def redirect_root
     if !user_signed_in? || current_user.kind == 0
+      redirect_to root_path
+    end
+  end
+
+  def show_redirect_root
+    @commission = Commission.find(params[:commission_id])
+    if !user_signed_in? || current_user.id != @commission.contractor_id && current_user.id != @commission.user_id
       redirect_to root_path
     end
   end
