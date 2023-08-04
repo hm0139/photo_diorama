54a90bc5b247109a9a0b91cdf398f3a114364295
hm0139 <m.kyohei0114@gmail.com>
2023-08-04 12:20:16 +0900

    現在取引中の依頼のリンクを設置

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,5 +2,6 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @achievement = @user.achievement if @user.kind == 1
+    @commissions = Commission.where(user_id: @user.id).or(Commission.where(contractor_id: @user.id)) if user_signed_in? && current_user.id == @user.id
   end
 end
